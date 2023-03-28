fc22a0888c7da43495f056a923eabb11b7c06546
KillerDoll12 <53497444+EricChelmu@users.noreply.github.com>
2023-03-28 18:46:57 +0200

    visualization added

---
+++ Dawn of the Undead/Assets/Scripts/AgentParameters.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralGeneration
+{
+    public class AgentParameters
+    {
+        public Vector3 position, direction;
+        public int length;
+    }
+}
