124e1bfaaac41768ba49ccb367758ec1ea33ea1f
Eric Chelmu <53497444+EricChelmu@users.noreply.github.com>
2023-10-10 13:15:18 +0200

    update

--- Dawn of the Undead/Assets/Scripts/Terrain Generation/MapGenerator.cs
+++ Dawn of the Undead/Assets/Scripts/Terrain Generation/MapGenerator.cs
@@ -6,6 +6,9 @@ namespace ProceduralGeneration
 {
     public class MapGenerator : MonoBehaviour
     {
+        public enum DrawMode {NoiseMap, ColourMap};
+        public DrawMode drawMode;
+
         public int mapWidth;
         public int mapHeight;
         public float noiseScale;
@@ -20,12 +23,38 @@ namespace ProceduralGeneration
 
         public bool autoUpdate;
 
+        public TerrainType[] regions;
+
         public void GenerateMap()
         {
             float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
+            Color[] colourMap = new Color[mapWidth * mapHeight];
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    float currentHeight = noiseMap[x, y];
+                    for (int i = 0; i < regions.Length; i++)
+                    {
+                        if (currentHeight <= regions[i].height)
+                        {
+                            colourMap[y * mapWidth + x] = regions[i].colour;
+                            break;
+                        }
+                    }
+                }
+            }
+
             MapDisplay display = FindObjectOfType<MapDisplay>();
-            display.DrawNoiseMap(noiseMap);
+            if (drawMode == DrawMode.NoiseMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+            }
+            else if (drawMode == DrawMode.ColourMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            }
         }
 
         private void OnValidate()
@@ -37,9 +66,14 @@ namespace ProceduralGeneration
             if (lacunarity < 1) lacunarity = 1;
 
             if (octaves < 0) octaves = 0;
-
-
         }
 
+        [System.Serializable]
+        public struct TerrainType
+        {
+            public string name;
+            public float height;
+            public Color colour;
+        }
     }
 }
